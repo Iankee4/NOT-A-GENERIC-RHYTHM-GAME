dbeb33b1f13addaf5a383845b6cfd85a409c1fbb
Iankee4 <iancarlovega8@gmail.com>
2024-12-18 20:02:06 -0400

    added Constants and variable types

---
+++ Constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+fps = 60
+bg_color = pygame.Color('black')
+screen_width = 900
+screen_height =900
+player_speed = 0
+screen = pygame.display.set_mode((screen_width, screen_height))
+green = pygame.Color('green')
+red = pygame.Color('red')
+blue = pygame.Color('blue')
+yellow = pygame.Color('yellow')
