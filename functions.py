b853e2dc580f7f0008ef66533bc170ca7c290df0
Iankee4 <iancarlovega8@gmail.com>
2024-12-19 15:19:11 -0400

    added functions to work on later

--- functions.py
+++ functions.py
@@ -5,3 +5,10 @@ def random_color():
     list_of_colors = [ red, blue, yellow]
     return random.choice(list_of_colors)
 
+def note_positions(): #to make the lines where the notes will fall down from
+    pass
+
+
+def draw_notes (): #draws the notes
+    pass
+
