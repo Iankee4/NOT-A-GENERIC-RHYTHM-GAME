ae02688ba849cbb10db504763f53c4d9e3bb4be7
Iankee4 <iancarlovega8@gmail.com>
2024-12-19 23:52:28 -0400

    menu start up thing

--- Main.py
+++ Main.py
@@ -3,3 +3,23 @@ from functions import *
 
 while True:
     main_menu()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            for i, rect in enumerate(menu_rects):
+                if rect.collidepoint(event.pos):
+                    if menu_items[i] == "Play":
+                        pass
+                        #level selection screen should open
+
+
+                    elif menu_items[i] == "Settings":
+                        pass
+                       # menu screen that should let you change keybind and stuff
+                    elif menu_items[i] == "Exit":
+                        quit()
+
+
+
