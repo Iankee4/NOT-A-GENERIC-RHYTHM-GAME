877a6aa570be610482c95231b80cf51f1e6fb95c
Iankee4 <iancarlovega8@gmail.com>
2024-12-20 17:17:29 -0400

    even more variables

--- Constants.py
+++ Constants.py
@@ -1,9 +1,9 @@
 import pygame
-
+pygame.init()
 fps = 60
 bg_color = pygame.Color('black')
-screen_width = 900
-screen_height =900
+screen_width = 600
+screen_height =600
 player_speed = 0
 screen = pygame.display.set_mode((screen_width, screen_height))
 green = pygame.Color('green')
@@ -14,7 +14,8 @@ white = pygame.Color('white')
 black = pygame.Color('black')
 gray = pygame.Color('gray')
 
-
-
-menu_items = ["Play", "Settings", "Exit"]
+font = pygame.font.Font(None, 125)
+highlight = (150, 150, 150)
+menu_items = ["Play", "Settings","Exit"]
 menu_rects = []
+total = len(menu_items) * 120
